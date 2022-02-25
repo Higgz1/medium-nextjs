64543992571fa08a1c9736789a20d6e960fe37af
Bryan Higgz <bryan.kahiga@gmail.com>
2022-02-25 15:19:20 +0300

    first commit

---
+++ pages/components/header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+function Header() {
+  return (
+    <header>
+        <div>
+            <Link href={"/"}>
+                <img src="http://links.papareact.com/yvf" alt="" />
+            </Link>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div></div>
+    </header>
+  )
+}
+
+export default Header
